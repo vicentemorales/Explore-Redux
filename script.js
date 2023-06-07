bb33309c5f04b8fb7e51ee3760e9d847b98f2f0a
vicentemorales <vmorales121@gmail.com>
2023-06-06 19:33:10 -0700

    added counter features using redux

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 const valueEl = document.getElementById('value')
 const plusBtn = document.getElementById('plus')
 const minusBtn = document.getElementById('minus')
+const plusFiveBtn = document.getElementById('plus-five')
+const minusFiveBtn = document.getElementById('minus-five')
+const incrementOdd = document.getElementById('increment-odd')
+const incrementAsync = document.getElementById('increment-async')
+const incrementCustomBtn = document.getElementById('increment-custom')
+const customValue = document.getElementById('custom-value')
 
 // initial state value
 const initialState = {
@@ -15,6 +21,12 @@ const counterReducer = (state = initialState, action) => {
             return { value: state.value + 1 }
         case 'counter/decremented':
             return { value: state.value - 1 }
+        case 'counter/incrementedFive':
+            return { value: state.value + 5 }
+        case 'counter/decrementedFive':
+            return { value: state.value - 5 }
+        case 'counter/custom':
+            return { value: state.value + action.payload }
         default:
         return state
     }
@@ -27,6 +39,14 @@ const addAction = {
 
 const subAction = {
   type: 'counter/decremented'
+}
+
+const addFiveAction = {
+  type: 'counter/incrementedFive'
+}
+
+const subFiveAction = {
+  type: 'counter/decrementedFive'
 }
 
 // generating the store
@@ -47,9 +67,38 @@ const subOne = () => {
   store.dispatch(subAction)
 }
 
+const addFive = () => {
+  store.dispatch(addFiveAction)
+}
+
+const subFive = () => {
+  store.dispatch(subFiveAction)
+}
+
+const incrementIfOdd = () => {
+  if(store.getState().value % 2 !== 0) {
+    store.dispatch(addAction)
+  }
+}
+
+const incrementOneSecond = () => {
+  setTimeout(() => {
+    store.dispatch(addAction)
+  }, 1000);
+}
+
+const changeCustom = () => {
+  store.dispatch({ type: 'counter/custom', payload: +customValue.value })
+}
+
 // event listeners
 plusBtn.addEventListener('click', addOne)
 minusBtn.addEventListener('click', subOne)
+plusFiveBtn.addEventListener('click', addFive)
+minusFiveBtn.addEventListener('click', subFive)
+incrementOdd.addEventListener('click', incrementIfOdd)
+incrementAsync.addEventListener('click', incrementOneSecond)
+incrementCustomBtn.addEventListener('click', changeCustom)
 
 // initial render
 render()
